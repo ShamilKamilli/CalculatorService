16a980c4f5552ea73e07018e28663c46d07bbddc
Shamilkamilli <shamil@code.edu.az>
2019-09-15 15:04:04 +0400

    dix: unit tests fixed

--- CalculationServiceRest/CalculationService.TESTs/Tests/CalculationServiceTest.cs
+++ CalculationServiceRest/CalculationService.TESTs/Tests/CalculationServiceTest.cs
@@ -15,30 +15,30 @@ namespace CalculationService.TESTs.Tests
         }
 
         [Fact]
-        public void Add()
+        public async Task Add()
         {
-            var response = await _requestSender.GetAsync("http://localhost:44386/api/calculate?firstNumber=5&secondNumber=4");
+            var response = await _requestSender.GetAsync($"{Constants.BaseUrl}/api/calculate?firstNumber=5&secondNumber=4");
             Assert.Equal("9", response);
         }
 
         [Fact]
         public async Task Substract()
         {
-            var response = await _requestSender.GetAsync("http://localhost:44386/api/calculate/subtract?firstNumber=9&secondNumber=4");
+            var response = await _requestSender.GetAsync($"{Constants.BaseUrl}/api/calculate/subtract?firstNumber=9&secondNumber=4");
             Assert.Equal("5", response);
         }
 
         [Fact]
         public async Task Multiply()
         {
-            var response = await _requestSender.GetAsync("http://localhost:44386/api/calculate/Multiply?firstNumber=7&secondNumber=8");
+            var response = await _requestSender.GetAsync($"{Constants.BaseUrl}/api/calculate/Multiply?firstNumber=7&secondNumber=8");
             Assert.Equal("56", response);
         }
 
         [Fact]
         public async Task Divide()
         {
-            var response = await _requestSender.GetAsync("http://localhost:44386/api/calculate/Divide?firstNumber=16&secondNumber=8");
+            var response = await _requestSender.GetAsync($"{Constants.BaseUrl}/api/calculate/Divide?firstNumber=16&secondNumber=8");
             Assert.Equal("2", response);
         }
     }
